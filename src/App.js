3193a35aca486f3bf0e2661db890b2414b541049
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-24 22:47:54 +0530

    Completed App

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import './assets/main.css'
+import ImageCard from './components/ImageCard'
+import ImageSearch from './components/ImageSearch'
+function App() {
+  const [images, setImages] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [serachTerm, setSearchTerm] = useState('')
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch(
+        `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${serachTerm}&image_type=photo&pretty=true`
+      ).then(res => res.json())
+      setImages(data.hits)
+      setIsLoading(false)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  /*
+  const fetchWrapper = useCallback(() => {
+    fetchData()
+  }, [fetchData])
+    */
+
+  useEffect(() => {
+    fetchData()
+    setIsLoading(false)
+  }, [setSearchTerm, fetchData])
+
+  return (
+    <div className="container mx-auto">
+      <ImageSearch setText={text => setSearchTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">Images not found</h1>
+      )}
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map(image => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}{' '}
+    </div>
+  )
+}
+
+export default App
