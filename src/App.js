c9d5b5f180527b22ad490f0bc1a4bc540ec7946c
pratham82 <mali.prathamesh82@gmail.com>
2021-09-16 14:53:34 +0530

    fixed fetchData issue

--- src/App.js
+++ src/App.js
@@ -5,34 +5,32 @@ import ImageSearch from './components/ImageSearch'
 function App() {
   const [images, setImages] = useState([])
   const [isLoading, setIsLoading] = useState(true)
-  const [serachTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
-  const fetchData = async () => {
-    try {
-      const data = await fetch(
-        `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${serachTerm}&image_type=photo&pretty=true`
-      ).then(res => res.json())
-      setImages(data.hits)
-      setIsLoading(false)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  /*
   const fetchWrapper = useCallback(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch(
+          `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`
+        ).then((res) => res.json())
+        setImages(data.hits)
+        setIsLoading(false)
+      } catch (err) {
+        console.log(err)
+      }
+    }
     fetchData()
-  }, [fetchData])
-    */
+  }, [searchTerm])
 
   useEffect(() => {
-    fetchData()
+    // fetchData()
+    fetchWrapper()
     setIsLoading(false)
-  }, [setSearchTerm, fetchData])
+  }, [setSearchTerm, fetchWrapper])
 
   return (
     <div className="container mx-auto">
-      <ImageSearch setText={text => setSearchTerm(text)} />
+      <ImageSearch setText={(text) => setSearchTerm(text)} />
       {!isLoading && images.length === 0 && (
         <h1 className="text-5xl text-center mx-auto mt-32">Images not found</h1>
       )}
@@ -40,7 +38,7 @@ function App() {
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
         <div className="grid grid-cols-3 gap-4">
-          {images.map(image => (
+          {images.map((image) => (
             <ImageCard key={image.id} image={image} />
           ))}
         </div>
